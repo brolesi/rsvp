9060238938bdb0f19cb41429a56af6e22855c42e
Fabio Fogliarini Brolesi <brolesi@gmail.com>
2010-10-26 18:58:45 -0200

    categorias e tags

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rsvp::Application.config.secret_token = 'c7fe70a4da9cc465c07f755d22952f941fe35aa83dc8de164909dddb6b816aa25c8b77445c4a50cf129cd6a329d092d6b1e88c3bc43c53f88a1b471b170adbe9'
+Rsvp::Application.config.secret_token = '0b817b5fc7adf4039abaf9218eec2f43cd8ae7abf173759decd19447b14f039cd112bf6f2c2e2d48747f3884cd6f3dc85d1c5d8bcd014ba8a876ddd4abfcd2dc'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rsvp::Application.routes.draw do
+  resources :tags
+
+  resources :categories
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
